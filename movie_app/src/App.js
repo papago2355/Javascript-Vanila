33b815fc0ac13b9aec309c6af61e031dacbb15e9
papago2355 <68721273+papago2355@users.noreply.github.com>
2021-05-10 10:45:43 +0900

    movie app by JS
    movie app by JS

---
+++ movie_app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./routes/Home";
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import Detail from './routes/Detail'
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail}/>
+    </HashRouter>
+  );
+}
+export default App;
